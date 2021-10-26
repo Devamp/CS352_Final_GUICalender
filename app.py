256dec0c34417cc8c82b229a251a3c4f911ba0d8
Devam Patel <Devam29@gmail.com>
2021-10-25 22:54:04 -0700

    big changes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from tkinter import *
+from tkinter.ttk import Style
 from tkcalendar import *
 
 
@@ -14,6 +16,7 @@ def verifyFile():
                 print("File opened sucessfully...")
                 print("Reading from file...")
                 print("")
+                print("### CALENDAR GUI IS OPEN ###")
                 flag = FALSE
                 return fileName
         else:
@@ -21,26 +24,51 @@ def verifyFile():
     
     myFile.close()
 
+
 # Read from the syllabus file
-def readFile(sentFile):
+def simulate(sentFile):
+    
+    # set up GUI window
+    root = Tk()
+    root.title("Event Calendar")
+    root.geometry("400x400")
+    root.configure(background= "#90E7FD")
+    
+    # Set up calendar to display
+    cal = Calendar(root, selectmode="day", year=2021, month=10,)
+    cal.pack(pady=20)
+
+    # open file in read mode
     myFile = open(sentFile, 'r')
-    print(myFile.read())
+    
+    # read line by line and create a calender event for each event
+    for line in myFile.readlines():
+        list = line.split("-")
+        #eventList = []
+        c = cal.calevent_create(datetime(int(list[2]),int(list[0]),int(list[1])), list[3], "Anything")
+        #eventList.append(c)
+
+    def returnEvent():
+        mylable.config(text="Event Due: " + cal.calevent_cget(c,"text"))
+        #mylable.config(text=cal.get_date())
+
+    # set up display button
+    displayButton = Button(root, text="Display", background="grey", command=returnEvent)
+    displayButton.pack(pady=20)
+
+    # set up output lable
+    mylable = Label(root, text="<Output Here>")
+    mylable.pack(pady=20)
+
+
+
+    root.mainloop() # end GUI
+
+
 
 def main():
     file = verifyFile()
-    readFile(file)
-
-    # set up GUI window
-    # root = Tk()
-    # root.title("My Syllabus Calendar")
-    # root.geometry("400x400")
-
-
-    # root.mainloop() # end GUI code
-
-
-
-
+    simulate(file)
 
 
 if __name__ == "__main__":
