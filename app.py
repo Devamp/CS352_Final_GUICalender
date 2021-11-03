465debddc1ca4d2248ce378345c6737caeb0ed15
Devam Patel <Devam29@gmail.com>
2021-11-02 20:48:59 -0700

    primary functionality works now

--- app.py
+++ app.py
@@ -16,14 +16,12 @@ def verifyFile():
                 print("File opened sucessfully...")
                 print("Reading from file...")
                 print("")
-                print("### CALENDAR GUI IS OPEN ###")
                 flag = FALSE
                 return fileName
         else:
             print("Incorrect file type. Program only reads .txt files!")
     
     myFile.close()
-
 
 # Read from the syllabus file
 def simulate(sentFile):
@@ -35,36 +33,52 @@ def simulate(sentFile):
     root.configure(background= "#90E7FD")
     
     # Set up calendar to display
-    cal = Calendar(root, selectmode="day", year=2021, month=10,)
+    cal = Calendar(root, selectmode="day", year=2021, month=12,)
     cal.pack(pady=20)
 
     # open file in read mode
     myFile = open(sentFile, 'r')
-    
-    # read line by line and create a calender event for each event
-    for line in myFile.readlines():
+
+    eventList = [] # stores list of events created
+    i = 0 # increment counter used for event ID
+
+    for line in myFile.readlines(): # read line by line and create a calender event for each event
         list = line.split("-")
-        #eventList = []
-        c = cal.calevent_create(datetime(int(list[2]),int(list[0]),int(list[1])), list[3], "Anything")
-        #eventList.append(c)
+        c = cal.calevent_create(datetime(int(list[3]),int(list[1]),int(list[2])), list[4], "Anything")
+        eventList.append(str(i) + " - " + (list[1] + "/" +list[2] + "/" + list[3].replace("2021","21"))) # format events
+        i = i+1  # increment event id
+    
+    def returnEvent(): # command function for display button
+        dateAsked = cal.get_date() # get selected date
 
-    def returnEvent():
-        mylable.config(text="Event Due: " + cal.calevent_cget(c,"text"))
-        #mylable.config(text=cal.get_date())
+        for date in eventList: # loop to find see if asked date matches event dates created from eventList
 
+            if (date[4:12] == dateAsked): # if the selected date matches the event dates
+                mylable.config(text="    Event: " + cal.calevent_cget(int(date[0]),"text")) # print asked event given the id
+                dueLable.config(text="  Due on: " + dateAsked)
+                
+    
+                
+                
+
+    
     # set up display button
     displayButton = Button(root, text="Display", background="grey", command=returnEvent)
     displayButton.pack(pady=20)
 
-    # set up output lable
-    mylable = Label(root, text="<Output Here>")
-    mylable.pack(pady=20)
+    # set up output lables
+    mylable = Label(root, text="Event Here")
+    dueLable = Label(root, text="Due Date Here")
 
+    mylable.pack()
+    dueLable.pack(pady=20)
 
+    dueLable.config(bg="white", width="20")
+    dueLable.config(font=("Arial", 12))
+    mylable.config(bg="white", width="40")
+    mylable.config(font=("Arial", 14))
 
-    root.mainloop() # end GUI
-
-
+    root.mainloop() # end GUI loop
 
 def main():
     file = verifyFile()
