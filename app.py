17c32a90ec64806dfce6ac7e0e7699b2b0b3b802
Devam Patel <Devam29@gmail.com>
2021-11-29 21:05:36 -0800

    some changes

--- app.py
+++ app.py
@@ -20,8 +20,6 @@ def verifyFile():
                 return fileName
         else:
             print("Incorrect file type. Program only reads .txt files!")
-    
-    myFile.close()
 
 # Read from the syllabus file
 def simulate(sentFile):
@@ -35,9 +33,8 @@ def simulate(sentFile):
     #print(root.keys())
     # Set up calendar to display
     cal = Calendar(root, selectmode="day", year=2021, month=12, bordercolor="black", selectbackground="red", 
-    headerbackground="white", )
-    cal.pack(pady=20)
-    print(cal.keys())
+    headerbackground="white",  )
+    cal.pack(pady=10)
 
     # open file in read mode
     myFile = open(sentFile, 'r')
@@ -48,26 +45,86 @@ def simulate(sentFile):
     for line in myFile.readlines(): # read line by line and create a calender event for each event
         list = line.split("-")
         c = cal.calevent_create(datetime(int(list[3]),int(list[1]),int(list[2])), list[4], "Anything")
-        eventList.append(str(i) + " - " + (list[1] + "/" +list[2] + "/" + list[3].replace("2021","21"))) # format events
+        tempDate = list[3]
+        eventList.append(str(i) + " - " + (list[1] + "/" +list[2] + "/" + list[3].replace(tempDate,tempDate[2:4]))) # format events
         i = i+1  # increment event id
     
     def returnEvent(): # command function for display button
         dateAsked = cal.get_date() # get selected date
-    
         for date in eventList: # loop to find see if asked date matches event dates created from eventList
+            
+            # This condition checks to see if cal events are > 10, if so we need to adjust the spacing
+            if (int(date[0:2]) >= 10):
+                compareDate = date[5:14]
+                twoDigits = True
+            else:
+                compareDate = date[4:12] 
+                twoDigits = False
 
-            if (date[4:12] == dateAsked): # if the selected date matches the event dates
-                mylable.config(text="    Event: " + cal.calevent_cget(int(date[0]),"text")) # print asked event given the id
-                dueLable.config(text="  Due on: " + dateAsked)
+            if (compareDate == dateAsked): # if the selected date matches the event dates
+                if (twoDigits):
+                    mylable.config(text="    Event: " + cal.calevent_cget(int(date[0:2]),"text")) # print asked event given the id
+                    dueLable.config(text="  Scheduled For: " + dateAsked)
+                elif(twoDigits == False):
+                    mylable.config(text="    Event: " + cal.calevent_cget(int(date[0]),"text")) # print asked event given the id
+                    dueLable.config(text="  Scheduled For: " + dateAsked)
                 break
             else :
                 mylable.config(text="    No events for selected date!") # print asked event given the id
                 dueLable.config(text="  ")
+                
             
-            
+    def del_event():
+        toDelete = cal.get_date()
+        for date in eventList:
+            # This condition checks to see if cal events are > 10, if so we need to adjust the spacing
+            if (int(date[0:2]) >= 10):
+                compareDate = date[5:14]
+                twoDigits = True
+            else:
+                compareDate = date[4:12] 
+                twoDigits = False
+
+            if (compareDate == toDelete): # if the selected date matches the event dates
+                if (twoDigits):
+                    cal.calevent_remove(int(date[0:2])) # delete the cal event
+                    eventList.remove(date) # and remove it from the events list
+
+                    mylable.config(text=" Event deleted successfully") # print asked event given the id
+                    dueLable.config(text="  ")
+
+                elif(twoDigits == False):
+                    cal.calevent_remove(int(date[0]))
+                    eventList.remove(date)
+
+                    mylable.config(text=" Event deleted successfully") # print asked event given the id
+                    dueLable.config(text="  ")
+                break
+
+    # def add_event():
+    #     dateToAdd = cal.get_date()
+    #     split = dateToAdd.split("/")
+    #     print(split)
+    #     c = cal.calevent_create(datetime(int(split[2]), int(split[0]), int(split[1])), "TEXT HERE", "Anything")
+    #     eventList.append(c)
+    #     t1 = Text(root)
+    #     t1.insert(END, "Input")
+
+  
     # set up display button
     displayButton = Button(root, text="Display", background="grey", command=returnEvent)
-    displayButton.pack(pady=20)
+    displayButton.pack(pady=5)
+    displayButton.pack(padx=0)
+
+    # # set up addEvent button
+    # addEventButton = Button(root, text="Add Event", background="grey", command=add_event)
+    # addEventButton.pack(pady=5)
+    # addEventButton.pack(padx=0)
+
+    # set up delete button
+    deleteButton = Button(root, text="Delete Event", background="grey", command=del_event)
+    deleteButton.pack(padx=0)
+    deleteButton.pack(pady=10)
 
     # set up output lables
     mylable = Label(root, text="Event Here")
